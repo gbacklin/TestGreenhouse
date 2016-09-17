16bd0b5d346945ef45922ebe82850d3f60810029
Gene Backlin <gbacklin@users.noreply.github.com>
2016-09-17 16:35:00 -0400

    Update add-key.sh

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -7,14 +7,14 @@ security default-keychain -s ios-build.keychain
 
 # Unlock the keychain
 security unlock-keychain -p travis ios-build.keychain
-
+echo '$PASSWORD'
 # Set keychain timeout to 1 hour for long builds
 # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./scripts/certs/DeveloperCertificates.p12 -k ~/Library/Keychains/ios-build.keychain -P $PASSWORD -T /usr/bin/codesign
-security import ./scripts/certs/DistributionCertificates.p12 -k ~/Library/Keychains/ios-build.keychain -P $PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/DeveloperCertificates.p12 -k ~/Library/Keychains/ios-build.keychain -P '$PASSWORD' -T /usr/bin/codesign
+security import ./scripts/certs/DistributionCertificates.p12 -k ~/Library/Keychains/ios-build.keychain -P '$PASSWORD' -T /usr/bin/codesign
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
