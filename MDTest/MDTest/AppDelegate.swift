0567543b62bd611b8f3be1f1db4238769f239730
Pathfinder <ebacklin@pathf.com>
2016-09-17 09:21:38 -0400

    Travis and Cert Doc in AppDelegate

--- MDTest/MDTest/AppDelegate.swift
+++ MDTest/MDTest/AppDelegate.swift
@@ -6,62 +6,56 @@
 //  Copyright © 2016 Gene Backlin. All rights reserved.
 //
 
-/*************************************************
- **           Code Signing with Travis
+/***************************************************
+ **           Code Signing with Travis            **
+ ***************************************************
  **
- http://stackoverflow.com/questions/27671854/travis-ci-fails-to-build-with-a-code-signing-error
- 
- No Codesigning
+ ** http://stackoverflow.com/questions/27671854/travis-ci-fails-to-build-with-a-code-signing-error
+ **
+ ***************************************************
+
+.travis.yml
  
  language: objective-c
- 
+ osx_image: xcode8
  script:
- - xcodebuild [DEFAULT_OPTIONS] CODE_SIGNING_REQUIRED=NO
- 
- With Codesigning
- 
- language: objective-c
- 
+ - xcodebuild -workspace TestGreenhouse.xcworkspace -scheme Release ONLY_ACTIVE_ARCH=NO
  before_script:
- - ./scripts/add-key.sh
+ - chmod a+x ./scripts/add-key.sh
+ - sh ./scripts/add-key.sh
+ # safelist
+ branches:
+ only:
+ - Swift-3.0
+ #This sends a slack notificatio to just me, you can configure easily
+ notifications:
+ slack: pathfindersoftware:rsq2vyDTwHR6wg54iVIpuH2u
+
+ ***************************************************
  
- script:
- - xcodebuild [DEFAULT_OPTIONS] CODE_SIGNING_REQUIRED=NO
- 
- -------
+ add-key.sh:
  
  #!/bin/sh
+ # Create a custom keychain
+ security create-keychain -p travis ios-build.keychain
  
- KEY_CHAIN=ios-build.keychain
- security create-keychain -p travis $KEY_CHAIN
- # Make the keychain the default so identities are found
- security default-keychain -s $KEY_CHAIN
+ # Make the custom keychain default, so xcodebuild will use it for signing
+ security default-keychain -s ios-build.keychain
+ 
  # Unlock the keychain
- security unlock-keychain -p travis $KEY_CHAIN
- # Set keychain locking timeout to 3600 seconds
- security set-keychain-settings -t 3600 -u $KEY_CHAIN
+ security unlock-keychain -p travis ios-build.keychain
+ 
+ # Set keychain timeout to 1 hour for long builds
+ # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
  
  # Add certificates to keychain and allow codesign to access them
- security import ./scripts/certs/dist.cer -k $KEY_CHAIN -T /usr/bin/codesign
- security import ./scripts/certs/dev.cer -k $KEY_CHAIN -T /usr/bin/codesign
- 
- security import ./scripts/certs/dist.p12 -k $KEY_CHAIN -P DISTRIBUTION_KEY_PASSWORD  -T /usr/bin/codesign
- security import ./scripts/certs/dev.p12 -k $KEY_CHAIN -P DEVELOPMENT_KEY_PASSWORD  -T /usr/bin/codesign
- 
- echo "list keychains: "
- security list-keychains
- echo " ****** "
- 
- echo "find indentities keychains: "
- security find-identity -p codesigning  ~/Library/Keychains/ios-build.keychain
- echo " ****** "
+ security import ./scripts/certs/DeveloperCertificates.p12 -k ~/Library/Keychains/ios-build.keychain -P Password123 -T /usr/bin/codesign
  
  # Put the provisioning profile in place
  mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
- 
- cp "./scripts/profiles/iOSTeam_Provisioning_Profile_.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
- cp "./scripts/profiles/DISTRIBUTION_PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
- 
+ cp "./scripts/profile/MDTest_Dev_Prov_Profile.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+
  *************************************************/
 
 
